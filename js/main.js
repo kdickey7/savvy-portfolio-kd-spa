d577f46c4413c012877ecdd0fe0f0efd63bdfcea
Keondra Dickey <kdickey08@gmail.com>
2017-08-31 19:43:30 -0500

    updated css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $('nav ul li').on('click', function(event){
   if(!$(event.target).hasClass('active')) {
-    
+
   }
 })
+
+
+
+var request = $.ajax( "https://jsonplaceholder.typicode.com/posts" );
+
+request.then(
+    ( data ) => console.log( data )
+);
+
+console.log('hello')
